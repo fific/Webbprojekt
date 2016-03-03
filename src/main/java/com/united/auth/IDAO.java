14bc4f0f2a3659aab875e4703a3dac08274652dd
jenfor <jenfor@student-chalmers.se>
2016-03-03 15:10:37 +0100

    IDAO

---
+++ src/main/java/com/united/auth/IDAO.java
@@ -0,0 +1,30 @@
+
+package com.united.auth;
+
+import java.util.List;
+
+/**
+ * Basic CRUD interface implemented by all DAO (Facade)
+ * classes 
+ *
+ * @author hajo
+ * @param <T> Type
+ * @param <K> Primary key (id)
+ */
+
+public interface IDAO<T, K> {
+
+    public void create(T t);
+
+    public void delete(K id);
+
+    public void update(T t);
+
+    public T find(K id);
+
+    public List<T> findAll();
+
+    public List<T> findRange(int first, int n );
+
+    public int count();
+}
